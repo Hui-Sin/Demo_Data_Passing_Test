0b14c4dfcaf7a9223601b3ed7a2283055fdb309f
Hui-Sin <20018354@myrp.edu.sg>
2021-05-21 10:57:15 +0800

    Part 2 halfway

--- app/src/main/java/sg/edu/rp/c346/id20018354/demodatapassingtest/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20018354/demodatapassingtest/MainActivity.java
@@ -2,13 +2,35 @@ package sg.edu.rp.c346.id20018354.demodatapassingtest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnPassInteger;
+    Button btnPassCharacter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnPassInteger = findViewById(R.id.buttonPassInteger);
+        btnPassCharacter=findViewById(R.id.buttonPassChar);
+        btnPassInteger.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra("value", 1);
+                startActivity(intent);
+            }
+        });
+        btnPassCharacter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ThirdActivity.class);
+                intent.putExtra("char", "");
+                startActivity(intent);
+            }
+        });
     }
 }
